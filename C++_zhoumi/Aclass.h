a43043b55d82d78d30b8d2508a30c625158f54a1
lingzerol <467264565@qq.com>
2018-03-19 07:34:12 +0800

    添加注释

--- C++_zhoumi/Aclass.h
+++ C++_zhoumi/Aclass.h
@@ -7,21 +7,21 @@ using namespace std;
 #endif
 class Aclass {
 public:
-	void  top_Five(string*);
-	void last_Five(string*);
-	float get_Average_score();
-	float get_Medium();
-	void information_Process();
-	void insert_Student(string);
-	Aclass();
+	void  top_Five(string*);//获得前五学生
+	void last_Five(string*);//获得倒数前五的学生
+	float get_Average_score();//获得平均成绩
+	float get_Medium();//获得成绩的中位数
+	void information_Process();//处理所有数据
+	void insert_Student(string);//插入学生
+	Aclass();//初始化变量
 private:
-	void average_process();
-	void middle_average();
-	string tfive[5];
-	string lfive[5];
-	float averagesc;
-	float maveragesc;
-	int snum;
-	int ssize;
-	Student *s;
+	void average_process();//求班级的平均成绩
+	void middle_average();//求总成绩的中位数
+	string tfive[5];//前五的学生
+	string lfive[5];//倒数前五的学生
+	float averagesc;//保存班级的平均成绩
+	float maveragesc;//保存成绩的中位数
+	int snum;//保存学生的数量
+	int ssize;//s数组的大小
+	Student *s;//用于保存学生成绩
 };
--- C++_zhoumi/Student.h
+++ C++_zhoumi/Student.h
@@ -7,25 +7,25 @@ using namespace std;
 #endif
 class Student {
 public:
-	float get_Term_score();
-	string get_Name();
-	bool information_Process(string);
-	Student();
+	float get_Term_score();//获得学生学期总成绩
+	string get_Name();//获得学生名字
+	bool information_Process(string);//处理学生数据
+	Student();//初始化变量
 	void output() {
 		cout << sname << ' ';
 		for (int i = 0; i < score_num; ++i) {
 			cout << score[i] << ' ';
 		}
 		cout << endl;
-	}
+	}//用于测试
 private:
    
-	float get_Medium();
-	float *score;
-	float mscore;
-	float lscore;
-	float tscore;
-	float medscore;
-	int score_num;
-	string sname;
+	float get_Medium();//获得成绩的中位数
+	float *score;//学生的所有成绩
+	float mscore;//期中成绩
+	float lscore;//期末成绩
+	float tscore;//学期总成绩
+	float medscore;//成绩的中位数
+	int score_num;//成绩数目
+	string sname;//学生名字
 };
